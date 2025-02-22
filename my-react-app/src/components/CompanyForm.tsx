57a10606bc458a19c3c1f4d9b9dbb567f55aee49
vibhasrik <vibhsrik@gmail.com>
2025-02-22 03:15:01 -0500

    Messed with a few things, centered components horizontally

--- my-react-app/src/components/CompanyForm.tsx
+++ my-react-app/src/components/CompanyForm.tsx
@@ -1,32 +1,29 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./Form.css";
 
 const CompanyForm: React.FC = () => {
-    const [companyName, setCompanyName] = useState('');
-    const history = useHistory();
+  const [companyName, setCompanyName] = useState("");
+  const history = useHistory();
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        // You can add additional logic here if needed
-        history.push('/questions');
-    };
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    history.push("/questions", { companyName });
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="companyName">Company Name:</label>
-                <input
-                    type="text"
-                    id="companyName"
-                    value={companyName}
-                    onChange={(e) => setCompanyName(e.target.value)}
-                    required
-                />
-            </div>
-            {/* Additional questions can be added here */}
-            <button type="submit">Next</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Company Name:
+        <input
+          type="text"
+          value={companyName}
+          onChange={(e) => setCompanyName(e.target.value)}
+        />
+      </label>
+      <button type="submit">Next</button>
+    </form>
+  );
 };
 
 export default CompanyForm;
